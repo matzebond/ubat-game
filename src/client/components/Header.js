706b7441a603a6220fab2a0a4b0a89c1be8b0be3
Matthias Schmitt <male.schmitt@posteo.de>
2017-03-23 02:00:13 +0100

    add fullscreen option

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,13 +1,35 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
+import screenfull from "screenfull";
 
 export default class Header extends React.Component {
-    render () {
+
+
+    toogleFullscreen() {
+        if (screenfull.enabled) {
+            screenfull.request();
+        }
+        else {
+            screenfull.exit();
+        }
+    }
+
+
+    handler(e) {
+        console.log(e);
+        screenfull.toggle();
+    }
+
+    render() {
         return (
-            <header>
-                {"Header: "}
-                <Link to="/">Home</Link>
+            <header style={{height: "25px"}}>
+                <p style={{float: "left"}}>
+                    {"Header: "}
+                    <Link to="/">Home</Link>
+                </p>
+                <p style={{float: "right"}}>
+                    <input type="checkbox" onClick={this.handler.bind(this)}/> fullscreen
+                </p>
             </header>
         );
     }
