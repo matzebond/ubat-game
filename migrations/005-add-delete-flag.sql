37fdf05d2d7612514cd5e437d5771801e1911f44
Matthias Schmitt <male.schmitt@posteo.de>
2018-01-06 21:33:08 +0100

    Fix 005 migration down part

--- migrations/005-add-delete-flag.sql
+++ migrations/005-add-delete-flag.sql
@@ -5,9 +5,19 @@
 ALTER TABLE entries
 ADD COLUMN deleted INTEGER DEFAULT 0;
 
-
 ----------------------------------------------------------
 -- Down
 ----------------------------------------------------------
 
+ALTER TABLE entries RENAME TO temp_entries;
 
+CREATE TABLE entries (
+        id INTEGER PRIMARY KEY,
+        text TEXT UNIQUE NOT NULL
+);
+
+INSERT INTO entries
+SELECT id, text
+FROM temp_entries;
+
+DROP TABLE temp_entries;
